787ed38c340cf3d346a8519b8582513c0f381237
RDeClercq <ruben2declercq@gmail.com>
2014-10-23 17:03:05 +0200

    Completed merge GPS and TempHumiButton. Not yet tested.

--- arduino/GPSTempHumiButton/GPSTempHumiButton.ino
+++ arduino/GPSTempHumiButton/GPSTempHumiButton.ino
@@ -12,6 +12,7 @@ Dezelfde methode is nodig om de library van de GPS-module te installeren. Deze f
 */
 //De GPS zit op pin 2 en pin 3, de TempHumi-sensor zit op pin 4, de Button op pin 5
 
+#include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
 #include <DHT11.h>
 const int TempHumiPin=4;
@@ -65,28 +66,60 @@ void loop(){
     }
   }
   if (timer > millis())  timer = millis();
-  if (millis() - timer > 2000) { 
-    timer = millis();
-    Serial.print("\nTime: ");
-    Serial.print(GPS.hour, DEC); Serial.print(':');
-    Serial.print(GPS.minute, DEC); Serial.print(':');
-    Serial.print(GPS.seconds, DEC); Serial.print('.');
-    Serial.println(GPS.milliseconds);
-    Serial.print("Date: ");
-    Serial.print(GPS.day, DEC); Serial.print('/');
-    Serial.print(GPS.month, DEC); Serial.print("/20");
-    Serial.println(GPS.year, DEC);
-    Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
-    if (GPS.fix) {
-      Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-      Serial.print(", "); 
-      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-      Serial.print("Angle: "); Serial.println(GPS.angle);
-      Serial.print("Altitude: "); Serial.println(GPS.altitude);
-      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+  
+  if (i%100==0){
+    int err;
+    float temp, humi;
+    if((err=dht11.read(humi, temp))==0)
+    {
+      Serial.print("th;");
+      Serial.print(temp);
+      Serial.print(";");
+      Serial.print(humi);
+      Serial.print(";");
+      Serial.println();
+    }else{
+      Serial.println();
+      Serial.print("Error No :");
+      Serial.print(err);
+      Serial.println();    
     }
   }
+  buttonState = digitalRead(ButtonPin);
+  if (buttonState == LOW) {
+    Serial.println("pb;1;");
+  }else{
+    Serial.println("pb;0;");
+  }
+  if (i%100==0){
+    if (millis() - timer > 2000) {
+      if (GPS.fix){
+        timer = millis();
+        Serial.print("gps;");
+        Serial.print(GPS.hour, DEC); Serial.print(':');
+        Serial.print(GPS.minute, DEC); Serial.print(':');
+        Serial.print(GPS.seconds, DEC); Serial.print('.');
+        Serial.print(GPS.milliseconds);
+        Serial.print(';');
+        Serial.print(GPS.day, DEC); Serial.print('/');
+        Serial.print(GPS.month, DEC); Serial.print("/20");
+        Serial.print(GPS.year, DEC);
+        Serial.print(';');
+        Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+        Serial.print(";"); 
+        Serial.print(GPS.longitude, 4); Serial.print(GPS.lon);
+        Serial.print(';');
+        Serial.println(GPS.speed);
+        Serial.print(';');
+        Serial.println(GPS.angle);
+        Serial.print(';');
+        Serial.println(GPS.altitude);
+        Serial.print(';');
+        //Serial.println((int)GPS.satellites);
+        //Serial.print(';');
+      }
+    }
+  }
+  delay(10);
+  i++;
 }
