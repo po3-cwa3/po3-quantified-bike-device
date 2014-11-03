d739436bfde35827756b7389e122c544b15d33c3
unknown <jaapjop95@gmail.com>
2014-11-03 18:06:09 +0100

    deleting int signal

--- arduino/BPM/BPM.ino
+++ arduino/BPM/BPM.ino
@@ -27,7 +27,6 @@ This update fixes the firstBeat and secondBeat flag usage so that realistic BPM 
 
 */
 
-
 //  VARIABLES
 int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
 int blinkPin = 13;                // pin to blink led at each beat
@@ -37,7 +36,6 @@ int BPM;                          // used to hold the pulse rate
 
 // these variables are volatile because they are used during the interrupt service routine!
 
-volatile int Signal;                // holds the incoming raw data
 volatile int IBI = 600;             // holds the time between beats, must be seeded! 
 volatile boolean Pulse = false;     // true when pulse wave is high, false when it's low
 volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
@@ -47,8 +45,6 @@ void setup(){
   Serial.begin(9600);             // we agree to talk fast!
   interruptSetup();                 // sets up to read Pulse Sensor signal every 2mS 
 }
-
-
 
 void loop(){
   if (QS == true){                       // Quantified Self flag is true when arduino finds a heartbeat
@@ -60,9 +56,3 @@ void loop(){
      }
   delay(20);                             //  take a break
 }
-
-
-
-
-
-
