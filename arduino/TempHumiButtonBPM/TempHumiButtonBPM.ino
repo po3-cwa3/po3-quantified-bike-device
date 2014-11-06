0518bca671ff4ce285e39ce3775149e97650b1fd
Floris Kint <floris.kint@gmail.com>
2014-11-06 17:08:49 +0100

    Other library for DHT (to avoid conflicts with the pulse sensor)

--- arduino/TempHumiButtonBPM/TempHumiButtonBPM.ino
+++ arduino/TempHumiButtonBPM/TempHumiButtonBPM.ino
@@ -1,14 +1,14 @@
+#include <DHT.h>
+
 /*
-Om de library te installeren, heb je de file DHT11.zip nodig.
-Deze is te vinden op https://drive.google.com/uc?export=download&id=0B3L4pZE60Jv0SzRsTTMxZGZNblU
+DHT-library: https://github.com/adafruit/DHT-sensor-library
 Klik nu in het arduinoprogramma op Sketch -> Import Library... -> Add Library...
 Vind de zip-file en voeg deze toe.
 Herstart nu het arduinoprogramma om de library volledig in te laden.
 */
 //De TempHumi-sensor zit op pin 5, de Button op pin 4, puls sensor op pin 0.
 //Voor code puls sensor (en referenties) zie BPM.ino. 
-
-#include <DHT11.h>
+#define DHTTYPE DHT11
 const int TempHumiPin = 8;
 const int ButtonPin = 7;
 
@@ -25,10 +25,11 @@ volatile int IBI = 600;
 volatile boolean Pulse = false;
 volatile boolean QS = false;
 
-DHT11 dht11(TempHumiPin);
+DHT dht(TempHumiPin, DHTTYPE);
 
 void setup(){
   Serial.begin(115200);
+  dht.begin();
   while (!Serial){;}
   pinMode(ButtonPin, INPUT);
   interruptSetup();
@@ -37,9 +38,10 @@ void setup(){
 void loop(){
   if (i%100==0){
     Serial.println("test");
-    int err;
+    /*int err;
     float temp, humi;
-    if((err=dht11.read(humi, temp))==0)
+    err=dht11.read(humi, temp);
+    if(err==0)
     {
       Serial.print("th;");
       Serial.print(temp);
@@ -52,14 +54,20 @@ void loop(){
       Serial.print("Error No :");
       Serial.print(err);
       Serial.println();    
-    }
+    }*/
+      Serial.print("th;");
+      Serial.print(dht.readTemperature());
+      Serial.print(";");
+      Serial.print(dht.readHumidity());
+      Serial.print(";");
+      Serial.println();
   Serial.println("after read");
   }
   buttonState = digitalRead(ButtonPin);
   if (buttonState == LOW) {
-      Serial.println("pb1;1;");
+      //Serial.println("pb1;1;");
     }else{
-      Serial.println("pb1;0;");
+      //Serial.println("pb1;0;");
     }
   if (QS == true){                      
       Serial.print("BPM;");
