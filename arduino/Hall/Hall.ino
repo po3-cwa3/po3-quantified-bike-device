9d20ae6b8e8d01e9d4ba071904d7ef28fec1d38f
unknown <jaapjop95@gmail.com>
2014-11-13 16:08:23 +0100

    whole new code and deleting directory

--- arduino/Hall/Hall.ino
+++ arduino/Hall/Hall.ino
@@ -1,26 +1,30 @@
-// nog niet af !!!
-// nog niet getest !!!
+int counter;
+float velocity = 0;
+float distance = 0;
+long time = 0;
+float radius = 33; //in cm
 
-int hallPin = 3;
-int hallState = 0;
-int radius = 33; //in cm
-int counter_1 = 0;
-int counter_2 = 0;
+void setup()
+ {
+   Serial.begin(9600);
+   attachInterrupt(0, counting, RISING);
+ }
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(hallPin, INPUT); 
-}
+void loop()
+ {
+   if (counter >= 5) {
+     distance = counter*2*radius*0.01*3.14;
+     velocity = (distance/(millis()-time))*3600; //in km/u
+     time = millis();
+     counter = 0;
+     Serial.print("km/h;");
+     Serial.print(velocity);
+     Serial.print(";");
+     Serial.println();
+   }
+ }
 
-void loop() {
-  hallState = digitalRead(hallPin);
-  ++counter_2;
-  if (hallState==HIGH){
-    time = (counter_2 - counter_1)/100;
-    counter_1 = counter_2
-    vel = ((2*radius*0.01*3.14)/time)*3.6 //in km/u
-    Serial.print("km/h;");
-    Serial.print(vel);
-    Serial.println();    
-  delay(10);
-}
+void counting()
+ {
+   counter++;
+ }
