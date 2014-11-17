a41093403dd20784d5b2ca1270f5faeb3589c48a
CWA3 <floris.kint@gmail.com>
2014-11-17 17:07:03 +0000

    Adds first non-working code to accelerometer

--- raspberry/service/images.py
+++ raspberry/service/images.py
@@ -1,38 +1,26 @@
 import string
 import random
-
-#import time
-#import picamera
-#import base64
-import json
-import requests
-
-images_path = "/home/pi/workspace/po3-quantified-bike-device/raspberry/service/images/"
-upload_url = "http://dali.cs.kuleuven.be:8080/qbike/upload"
-
-
 def id_generator(size=20,chars=string.ascii_uppercase+string.ascii_lowercase+string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-
-def get_filename(photo_id):
-    return photo_id+".jpg"
-
-
+import time
+import picamera
 def take_photo():
     photo_id=id_generator()
+    destination='/home/pi/workspace/po3-quantified-bike-device/raspberry/service/images/'+photo_id+'.jpg'
     with picamera.PiCamera() as camera:
-        camera.capture(images_path+get_filename(photo_id))
+        camera.capture(destination)
     return photo_id
 
-
+import base64
+import json
+import requests
 def send_to_server(photo_id,trip_id,user_id):
-    location=images_path+get_filename(photo_id)
+    location='/home/pi/workspace/po3-quantified-bike-device/raspberry/service/images/'+photo_id+'.jpg'
     f=open(location,"rb").read().encode("base64")
-    test=json.dumps({"imageName":get_filename(photo_id),"tripID":trip_id,"userID":user_id,"raw":f})
-    url=upload_url
+    test=json.dumps({"imageName":photo_id+'.jpg',"tripID":trip_id,"userID":user_id,"raw":f})
+    url="http://dali.cs.kuleuven.be:8080/qbike/upload"
+    data=test
     headers={'Content-type':'application/json','Accept':'text/plain'}
-    requests.post(url,data=test,headers=headers)
-    return get_filename(photo_id)
-
-# test_trip_id "5436a08271b56f091b616920","r0463107"
+    r=requests.post(url,data=test,headers=headers)
+    return "Done!"
