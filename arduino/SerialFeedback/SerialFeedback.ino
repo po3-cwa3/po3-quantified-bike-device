eb9c32f87774927787da045aaa258b8a722cf27e
unknown <jaapjop95@gmail.com>
2014-11-27 15:03:03 +0100

    initial files

---
+++ arduino/SerialFeedback/SerialFeedback.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  if (Serial.available()>0){
+    char incoming = Serial.read()
+    char data[2]
+    data[0] = incoming
+    data[1] = '\0'
+    Serial.println(data);
+  }
+}
