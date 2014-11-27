406b75a7940fc60a8787ffb4d560b771d882741e
unknown <jaapjop95@gmail.com>
2014-11-27 15:15:55 +0100

    feedback

--- arduino/SerialFeedback/SerialFeedback.ino
+++ arduino/SerialFeedback/SerialFeedback.ino
@@ -4,10 +4,10 @@ void setup() {
 
 void loop() {
   if (Serial.available()>0){
-    char incoming = Serial.read()
-    char data[2]
-    data[0] = incoming
-    data[1] = '\0'
+    char incoming = Serial.read();
+    char data[2];
+    data[0] = incoming;
+    data[1] = '\0';
     Serial.println(data);
   }
 }
