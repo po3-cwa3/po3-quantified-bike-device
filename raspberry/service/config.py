38fc9dda45227182b22c768415ccc94b347e858a
Floris Kint <floris.kint@gmail.com>
2014-12-04 11:23:30 +0100

    Untrack config.py

--- raspberry/service/config.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'fkint'
-
-remote_hostname = "dali.cs.kuleuven.be"
-remote_port = 8080
-local_host = 'localhost'
-local_database_username = 'QB_CWA3'
-local_database_password = 'CEeT9cPFSnPExMzQ'
-local_database_name = 'QuantifiedBike'
-user_id = 'r0451433'
