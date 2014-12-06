bdabbd781978d6d8dd26d735f47e28c2a5559e4a
Floris Kint <floris.kint@gmail.com>
2014-12-06 16:18:55 +0100

    Add stop-trip debugging information

--- raspberry/service/data_store.py
+++ raspberry/service/data_store.py
@@ -144,23 +144,25 @@ class DatabaseConnection:
         Adds a new trip to the local database.
         :param trip: a reference to the Trip object. This object will be notified about the id it receives in the database.
         """
-        t = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
-        query = "INSERT INTO Trips (StartTime, EndTime) VALUES('"+t+"', '"+t+"')"
-        cursor = self.db.cursor()
-        cursor.execute(query)
-        trip.set_id(cursor.lastrowid)
-        self.db.commit()
+        with closing(self.db.cursor()) as cursor:
+            t = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
+            query = "INSERT INTO Trips (StartTime, EndTime) VALUES('"+t+"', '"+t+"')"
+            #cursor = self.db.cursor()
+            cursor.execute(query)
+            trip.set_id(cursor.lastrowid)
+            self.db.commit()
 
     def stop_trip(self, trip_id):
         """
         Sets the end time of the trip.
         :param trip_id: the id of the trip of which the end time should be set.
         """
-        t = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
-        query = "UPDATE Trips SET EndTime='"+t+"' WHERE Id = '"+str(trip_id)+"'"
-        cursor = self.db.cursor()
-        cursor.execute(query)
-        self.db.commit()
+        print "stopping trip in data_store"
+        with closing(self.db.cursor()) as cursor:
+            t = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
+            query = "UPDATE Trips SET EndTime='"+t+"' WHERE Id = '"+str(trip_id)+"'"
+            cursor.execute(query)
+            self.db.commit()
 
 
     def send_data(self, data, trip_id):
